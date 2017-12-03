4e44424afb83a45ce0f6854b8c6b3f4b6710b1d9
Jtannhau <32885506+Jtannhau@users.noreply.github.com>
2017-12-03 18:48:14 -0500

    J.Crowley_Final Project_Seudo JQuery 12/4

--- J.Crowley Final Project/js/main.js
+++ J.Crowley Final Project/js/main.js
@@ -1,1 +1,79 @@
 // Make it rain!
+
+//when the user hovers on the see more button under north america
+  //add css class to change color of the se more button to dark purple
+
+//When user clicks the see more button under north america
+  //show photos in north america
+  //hide photos in other galleries
+
+  //when user clicks on next within photos in north america
+    //remove css class to increase size of the current photo
+    //shift photos to the right
+    //add css class to increase size of next photo
+
+  //when user clicks on back within photos in north america
+    //remove CSS class to increase size of the current photo
+    //shift photos to the left
+    //add css class to increase size of the previous photo
+
+//when the user hovers on the see more button under south america
+  //add css class to change color of the see more button to dark purple
+
+//When the user clicks the see more button under south america
+  //show photos in south america
+  //hide photos in other galleries
+
+    //when user clicks on next within photos in south america
+      //remove css class to increase size of the current photo
+      //shift photos to the right
+      //add css class to increase size of next photo
+
+    //when user clicks on back within photos in south america
+      //remove CSS class to increase size of the current photo
+      //shift photos to the left
+      //add css class to increase size of the previous photo
+
+//when the user hovers on the see more button under Europe
+    //add css class to change color of the see more button to dark purple
+
+//When the user clicks the see more button under Europe
+  //show photos in Europe
+  //hide photos in other galleries
+
+    //when user clicks on next within photos in Europe
+      //remove css class to increase size of the current photo
+      //shift photos to the right
+      //add css class to increase size of next photo
+
+    //when user clicks on back within photos in Europe
+      //remove CSS class to increase size of the current photo
+      //shift photos to the left
+      //add css class to increase size of the previous photo
+
+
+//when the user hovers on the see more button under Asia
+  //add css class to change color of the see more button to dark purple
+
+//When the user clicks the see more button under Asia
+  //show photos in Asia
+  //hide photos in other galleries
+
+    //when user clicks on next within photos in Asia
+      //remove css class to increase size of the current photo
+      //shift photos to the right
+      //add css class to increase size of next photo
+
+    //when user clicks on back within photos in Asia
+      //remove CSS class to increase size of the current photo
+      //shift photos to the left
+      //add css class to increase size of the previous photo
+
+//when the user hovers on subscribe button
+  //add css class to change color of the subscribe button
+
+//when the user clicks subscribe
+  // show success modal
+
+//when the user clicks the x in the success modal
+  // hide the success modeal
