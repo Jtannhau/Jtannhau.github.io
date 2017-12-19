f57e0a25c01ffdf574cbf98a2cddff37293efdbd
Jtannhau <32885506+Jtannhau@users.noreply.github.com>
2017-12-18 19:43:32 -0500

    J.Crowley Final Project (v.FINAL)

--- J.Crowley Final Project/js/southamerica.js
+++ J.Crowley Final Project/js/southamerica.js
@@ -30,17 +30,3 @@ $('#previous').on('click', function() {
   $('img').hide();
   $('img').eq(imageNumber).fadeIn(300);
 });
-
-
-setInterval(function () {
-  if (imageNumber < 5) {
-    imageNumber++;
-  } else {
-    imageNumber = 0;
-  }
-
-  console.log(imageNumber);
-
-  $('img').hide();
-  $('img').eq(imageNumber).fadeIn(300);
-}, 6000);
